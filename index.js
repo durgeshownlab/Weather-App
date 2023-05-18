9789856798cf40326947fab41efab21d85cf03ce
durgeshownlab <105972298+durgeshownlab@users.noreply.github.com>
2023-05-18 13:26:27 +0530

    initial commit
    just pushing my code on github

---
+++ index.js
@@ -0,0 +1,45 @@
+const http = require("http");
+const fs = require("fs");
+const requests = require("requests");
+
+const homeFile = fs.readFileSync("index.html", "utf-8");
+
+const replaceVal = (tempVal, orgVal)=>{
+    let temperature=tempVal.replace("{%tempval%}", orgVal.main.temp)
+    temperature=temperature.replace("{%tempmin%}", orgVal.main.temp_min)
+    temperature=temperature.replace("{%tempmax%}", orgVal.main.temp_max)
+    temperature=temperature.replace("{%city%}", orgVal.name)
+    temperature=temperature.replace("{%country%}", orgVal.sys.country)
+    temperature=temperature.replace("{%tempStatus%}", orgVal.weather[0].main)
+
+    return temperature;
+}
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+
+    requests("https://api.openweathermap.org/data/2.5/weather?q=imphal&units=metric&appid=fdb876aac9705866d0d36932f09effe1")
+
+      .on("data", function (chunk) {
+        const objData=JSON.parse(chunk);
+        const arrData=[objData];
+        console.log(arrData[0]);
+        const realTimeData=arrData
+            .map((val)=> replaceVal(homeFile, val))
+            .join("");
+        res.write(realTimeData);
+        // res.writeHead(200);
+        // console.log(realTimeData);
+      })
+
+      .on("end", function (err) {
+        if (err) return console.log("connection closed due to errors", err);
+        
+        res.end();
+      });
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("server is listening at port number 8000 127.0.0.1:8000");
+});
